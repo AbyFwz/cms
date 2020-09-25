cf76f94885d7829f61ea80cc0b75d271928500b0
Abyan Fawwaz <abyanfawwaz77@gmail.com>
2020-09-25 09:56:24 +0700

    Update 1.1

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use Illuminate\Support\Facades\Cache;
 
 class ArticleController extends Controller
 {
     public function __invoke($id)
     {
+        // Cache::rememberForever('article:id:$id', function() use ($id) {
+        //     return Article::find($id);
+        // });
+        // $article = Cache::get('article:id:$id');
         $article = Article::find($id);
         $article = json_decode(json_encode($article));
 
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Article;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        $articlesAll = Article::all();
+        Cache::remember('articlesAll', 15, function(){
+            return Article::all();
+        });
+        $articlesAll = Cache::get('articlesAll');
         $articlesAll = json_decode(json_encode($articlesAll));
 
         return view('homeViews')->with(compact('articlesAll'));
